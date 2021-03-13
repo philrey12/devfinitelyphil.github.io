4643bb2b9b1c8f878de209f6746d9c8a4e0c3cf7
philrey12 <phil.raquinel@gmail.com>
2021-03-13 16:54:30 +0800

    Update Nav and Home section

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+// MENU
+const navMenu = document.getElementById('nav-menu'), 
+    toggleMenu = document.getElementById('nav-toggle'), 
+    closeMenu = document.getElementById('nav-close');
+
+// show
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+});
+
+// hide
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+});
+
+// REMOVE MENU
+const navLink = document.querySelectorAll('.nav_link');
+
+const linkAction = () => {
+    navMenu.classList.remove('show')
+};
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll(section[id]);
+
+window.addEventListener('scroll', scrollActive);
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(`.nav_menu a[href*=${sectionId}]`).classList.add('active');
+        } else {
+            document.querySelector(`.nav_menu a[href*=${sectionId}]`).classList.remove('active');
+        }
+    });
+}
